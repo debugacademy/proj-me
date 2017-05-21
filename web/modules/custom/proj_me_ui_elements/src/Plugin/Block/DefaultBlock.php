1556699dd2f82f2fab4dcbfbb807e0eaba04557c
Robert Foley <robertfoleyjr@gmail.com>
2017-05-21 12:02:35 -0700

    module - proj_me_ui_elements

---
+++ web/modules/custom/proj_me_ui_elements/src/Plugin/Block/DefaultBlock.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Drupal\proj_me_ui_elements\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+
+/**
+ * Provides a 'DefaultBlock' block.
+ *
+ * @Block(
+ *  id = "default_block",
+ *  admin_label = @Translation("Default block"),
+ * )
+ */
+class DefaultBlock extends BlockBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+    $build = [];
+    $build['default_block']['#markup'] = 'Implement DefaultBlock.';
+
+    return $build;
+  }
+
+}
